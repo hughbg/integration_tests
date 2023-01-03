0847ff8b31a190ce0ffd7abcf6616ab222770f82
Hugh Garsden <hugh.garsden@manchester.ac.uk>
2023-01-03 14:59:21 -0700

    More plots

---
+++ papermill.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#SBATCH --job-name='paper_run'
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=232GB
+#SBATCH --output=paper_run_%j.log
+#SBATCH --time=24:00:00
+#SBATCH -p hera
+
+
+if [ -z $1 ]
+then
+  echo No input args
+  exit
+fi
+
+if [ -z $2 ]
+then  
+  output=`echo $1 | sed s/\.ipynb/_out\.ipynb/`
+else
+  output=$2
+fi
+
+source  ~/miniconda3/etc/profile.d/conda.sh; conda activate frf_paper
+
+
+date
+
+papermill $1 $output
+
+
+date
